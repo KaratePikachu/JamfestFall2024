86ed551e436d3735b83b91fe0fbd90ddb9e1eb3a
Bananaz21 <72627687+cooki-chan@users.noreply.github.com>
2024-11-08 16:47:36 -0500

    fixes

--- Player/player.gd
+++ Player/player.gd
@@ -1,9 +1,9 @@
 class_name Player extends CharacterBody2D
 
-const WALK_FORCE = 600
-const WALK_MAX_SPEED = 200
-const STOP_FORCE = 1300
-const JUMP_SPEED = 200
+const WALK_FORCE = 1000
+const WALK_MAX_SPEED = 400
+const STOP_FORCE = 1800
+const JUMP_SPEED = 400
 
 static var instance : Player
 
