49a70fcd2a9c4de3979db2a18d0bfed72208cd25
Brendan Halstead <Brendan_Halstead@live.com>
2024-11-08 21:56:35 -0500

    lidar scene

--- LIDAR/ray_cast_2d.gd
+++ LIDAR/ray_cast_2d.gd
@@ -9,3 +9,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			var screen_coord = get_viewport().get_screen_transform() * get_global_transform_with_canvas() * position
+			#(get_viewport().get_visible_rect().position - global_position)
+			var dir : Vector2 = event.position - screen_coord
+			rotation = rad_to_deg(dir.angle())
+			#rotation = position.angle_to_point(event.position) * 2 * PI
+			print("ray position: ", screen_coord)
+			print("Mouse Motion at: ", event.position)
+			print("ray rotation: ", rotation)
