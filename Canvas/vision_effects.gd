ae8b778667b961de753af6f6facec69baab6d8ea
KaratePikachu <KaidenKeyZ@gmail.com>
2024-11-08 19:55:56 -0500

    Commented Code to make it work

--- Canvas/vision_effects.gd
+++ Canvas/vision_effects.gd
@@ -1,9 +1,9 @@
 extends CanvasLayer
 
-
-# Called when the node enters the scene tree for the first time.
-func _init() -> void:
-	Player.instance.connect("glasses_changed", )
-	
-func change_vision_effect() -> void:
+#
+## Called when the node enters the scene tree for the first time.
+#func _init() -> void:
+	#Player.instance.connect("glasses_changed", )
+	#
+#func change_vi	sion_effect() -> void:
 	
