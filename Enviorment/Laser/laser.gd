6d36d9206697da907e3595e2115fb66144669ed7
Bananaz21 <72627687+cooki-chan@users.noreply.github.com>
2024-11-09 03:53:29 -0500

    laser forgotten features

--- Enviorment/Laser/laser.gd
+++ Enviorment/Laser/laser.gd
@@ -4,20 +4,27 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Player.instance.glasses_changed.connect(_on_glasses_change)
 	$Timer.wait_time = loop_time
 
 func _on_body_entered(body: Node2D) -> void:
 	print("AAAAAAAAAAAAAAAA")
 	if body is Player:
 		body.set_global_position(Vector2(0,0))
+		
+func _on_glasses_change(old_glasses: int, new_glasses: int) -> void:
+	if(new_glasses == Player.Glasses.INFARED or Player.Glasses.NORMAL):
+		$ColorRect.visible = true
+	else: 
+		$ColorRect.visible = false
 
 
 func _on_timer_timeout() -> void:
 	$Shape.set_deferred("disabled", !$Shape.disabled)
 	
 	if $Shape.disabled:
-		$Shape/ColorRect.self_modulate.a = 1
+		$ColorRect.self_modulate.a = 1
 	else: 
-		$Shape/ColorRect.self_modulate.a = 0.2
+		$ColorRect.self_modulate.a = 0.2
 		
 	
