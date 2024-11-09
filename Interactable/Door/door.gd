e9e5130f0be6b337539a3f9699ba8d2ec0564dcf
Bananaz21 <72627687+cooki-chan@users.noreply.github.com>
2024-11-08 21:59:28 -0500

    temp save

--- Interactable/Door/door.gd
+++ Interactable/Door/door.gd
@@ -1,20 +1,19 @@
 extends StaticBody2D
 
 @export var open := false
-@export var color := Color(0,0,0)
+@export var color : Color
 
 func _on_ready() -> void:
 	$DoorSprite.color = color
-
 # Called when the node enters the scene tree for the first time.
 func _on_triggered() -> void:
 	print("WAHHHHHHHH")
 	open = !open
 	if(open):
-		$DoorSprite.modulate.a = 100
+		$DoorSprite.self_modulate.a = 0.2
 		$CollisionArea.set_deferred("disabled", true)
 	else:
-		$DoorSprite.modulate.a = 20
+		$DoorSprite.self_modulate.a = 1.0
 		$CollisionArea.set_deferred("disabled", false)
 		
 	
