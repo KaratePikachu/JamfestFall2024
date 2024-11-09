eb3103103fbbdf798824a02d9fd02d454abfb39c
Brendan Halstead <Brendan_Halstead@live.com>
2024-11-09 03:00:05 -0500

    Drone post processing

--- Canvas/vision_effects.gd
+++ Canvas/vision_effects.gd
@@ -13,12 +13,14 @@ func change_vision_effect(old_glasses : Player.Glasses, new_glasses : Player.Gla
 func disable_vision():
 	$Blindness.visible = false
 	$Sunglasses.visible = false
+	$Drone.visible = false
 	
 func enable_vision(glasses : Player.Glasses):
 	if(glasses == Player.Glasses.NONE):
 		$Blindness.visible = true
 	elif(glasses == Player.Glasses.SUNGLASSES):
 		$Sunglasses.visible = true
-				
+	elif(glasses == Player.Glasses.DRONE):
+		$Drone.visible = true
 	pass
 	
