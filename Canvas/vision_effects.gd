fe38c5f0888a17a1dad361562086313aa3113730
KaratePikachu <KaidenKeyZ@gmail.com>
2024-11-08 19:52:07 -0500

    Name Specification of Vision Effects Canvas

---
+++ Canvas/vision_effects.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _init() -> void:
+	Player.instance.connect("glasses_changed", )
+	
+func change_vision_effect() -> void:
+	
