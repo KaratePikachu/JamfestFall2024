1e6c45bfae13875af675f59a20dcdaeef5276d0b
Bananaz21 <72627687+cooki-chan@users.noreply.github.com>
2024-11-10 05:10:17 -0500

    extremely basic intro

--- Enviorment/ForcedGlassesChange/glasses_change.gd
+++ Enviorment/ForcedGlassesChange/glasses_change.gd
@@ -1,24 +1,12 @@
 extends Area2D
 
-@export var text : String = ""
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$Label.text = text
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@export var forced : Player.Glasses = Player.Glasses.NONE
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		var tween = get_tree().create_tween()
-		tween.tween_property($Label, "visible_characters", text.length(), 1.5)
+		Player.instance.switch_glasses(forced)
 
 
 func _on_body_exited(body: Node2D) -> void:
-	if body is Player:
-		$Label.visible_characters = 0
+	pass
--- Enviorment/TextPopup/text_popup.gd
+++ Enviorment/TextPopup/text_popup.gd
@@ -1,12 +1,24 @@
 extends Area2D
 
-@export var forced : Player.Glasses = Player.Glasses.NONE
+@export var text : String = ""
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Label.text = text
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		Player.instance.worn_glasses = forced
+		var tween = get_tree().create_tween()
+		tween.tween_property($Label, "visible_characters", text.length(), 1.5)
 
 
 func _on_body_exited(body: Node2D) -> void:
-	pass
+	if body is Player:
+		$Label.visible_characters = 0
